bae338b9ded05a1eb4f16f3016348bd3ff50b0f0
Dejan Ribič <dejan.ribic@gmail.com>
2025-10-17 14:18:25 +0200

    New tree-sitter working.
    Setting lazy = false,
    for nvim-treesitter.
    
    Signed-off-by: Dejan Ribič <dejan.ribic@gmail.com>

--- init.lua
+++ init.lua
@@ -957,28 +957,66 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'go', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'rust', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
+    branch = 'main',
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
+    config = function()
+      ---@param buf integer
+      ---@param language string
+      ---@return boolean
+      local function treesitter_attach(buf, language)
+        -- check if parser exists before starting highlighter
+        if not vim.treesitter.language.add(language) then
+          return false
+        end
+        -- enables syntax highlighting and other treesitter features
+        vim.treesitter.start(buf, language)
+
+        -- enables treesitter based folds
+        -- for more info on folds see `:help folds`
+        -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+        -- enables treesitter based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        return true
+      end
+
+      local available_parsers = require('nvim-treesitter.config').get_available()
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local buf, filetype = args.buf, args.match
+          local language = vim.treesitter.language.get_lang(filetype)
+          if not language then
+            return
+          end
+
+          if not (treesitter_attach(buf, language) or vim.tbl_contains(available_parsers, language)) then
+            return
+          end
+
+          -- automaically install parser for missing languages
+          -- attempt to install even if it is available accoring to `vim.treesitter.langauge.add()`,
+          -- to ensure the latest version is installed using `nvim-treesitter`, instead of the outdated vendored parser
+          if not vim.tbl_contains(require('nvim-treesitter.config').get_installed 'parsers', language) then
+            -- attempt to start highlighter after installing missing language
+            require('nvim-treesitter.install').install(language):await(function()
+              treesitter_attach(buf, language)
+            end)
+          end
+        end,
+      })
+
+      -- ensure basic parser are installed
+      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'zig', 'python', 'go', 'rust' }
+      require('nvim-treesitter').install(parsers)
+    end,
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects (using the 'main' branch)
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
