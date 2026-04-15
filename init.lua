12e5b68b057b9c08e54bc6380bd15e3c9a080e94
Stattek <107233826+Stattek@users.noreply.github.com>
2026-04-15 03:49:09 -0500

    Fix descriptions of gitsigns keymappings (#1997)
    * Fix descriptions of keymappings
    
    * Fix capitalization
    
    * Prep for next patch(changes will be reverted after)
    
    Signed-off-by: Dejan Ribič <dejan.ribic@gmail.com>

--- init.lua
+++ init.lua
@@ -707,10 +707,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
-        go = { 'goimports' },
-        zig = { 'zig fmt' },
-        bash = { 'bash-language-server' },
+        -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
