14fef4da138a42826cd5e7052d54607fe0b497ba
Dejan Ribič <dejan.ribic@gmail.com>
2026-05-08 16:59:30 +0200

    Setting kanagawa-dragon as colorscheme.
    Signed-off-by: Dejan Ribič <dejan.ribic@gmail.com>

--- init.lua
+++ init.lua
@@ -388,6 +388,12 @@ do
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   vim.pack.add { gh 'folke/tokyonight.nvim' }
+  -- Additional colorschemes (they have to be installed here for the colorscheme command to work.)
+  vim.pack.add {
+    gh 'rebelot/kanagawa.nvim',
+    gh 'rose-pine/neovim',
+    gh 'dasupradyumna/midnight.nvim',
+  }
   ---@diagnostic disable-next-line: missing-fields
   require('tokyonight').setup {
     styles = {
@@ -398,7 +404,7 @@ do
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'kanagawa-dragon'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
